2712f589434d4446941bb6794a20276c806d5d4f
itsJustAbs <ardabaransaral6@gmail.com>
2024-02-03 12:54:47 +0300

    home view & login page

---
+++ lib/view/home_view.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import '../models/user_model.dart';
+import '../repository/user_repo.dart';
+
+
+class HomeView extends StatelessWidget {
+  final String uid;
+  const HomeView({super.key,required this.uid});
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    return  Scaffold(
+      body: FutureBuilder<UserModel?>(
+          future: UserRepository().getUser(uid),
+          builder: (context, snapshot) {
+            if(snapshot.connectionState == ConnectionState.waiting){
+              return Center(child: CircularProgressIndicator());
+            }else {
+
+              return Text("User name is ${snapshot.data?.name}");
+            }
+          }
+    ),
+    );
+  }
+}
